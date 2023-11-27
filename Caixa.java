c6fc6b47c70fc5e9419028c6a38393329ada3575
Paulo Rodrigues <paulo.rodrigues@grupoicts.com.br>
2023-11-27 19:16:04 -0400

    first commit

---
+++ Caixa.java
@@ -0,0 +1,38 @@
+public class Caixa {
+    private String Modelo;
+    private String Tipo;
+    private Integer Tamanho;
+    private Double Valor;
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public Integer getTamanho() {
+        return Tamanho;
+    }
+
+    public void setTamanho(Integer tamanho) {
+        Tamanho = tamanho;
+    }
+
+    public Double getValor() {
+        return Valor;
+    }
+
+    public void setValor(Double valor) {
+        Valor = valor;
+    }
+}
